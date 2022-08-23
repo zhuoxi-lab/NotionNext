0c9e12e249ce63b1cd877b642cb11be2b7946a04
MelogyShu <111693855+MelogyShu@users.noreply.github.com>
2022-08-23 09:13:30 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,7 @@ const BLOG = {
   LINK: 'https://melody.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
-  process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
+  process.env.NOTION_PAGE_ID || '6fb513eb521c4abc8e209ce8568fc7d9', // Important page_id！！！Duplicate Template from https://almondine-carp-0eb.notion.site/6fb513eb521c4abc8e209ce8568fc7d9?v=4faa330d4a0540d996ea790d3b6204f5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
   DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
 
