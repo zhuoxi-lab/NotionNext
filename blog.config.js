b17a26d819db2d6054491dc069ac57643e93a35d
MelogyShu <111693855+MelogyShu@users.noreply.github.com>
2022-08-24 08:27:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
-  AUTHOR: '去雨曲', // 作者
+  AUTHOR: '砌玉曲', // 作者
   BIO: '一曲新词酒一杯', // 作者简介
   LINK: 'https://melody.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
